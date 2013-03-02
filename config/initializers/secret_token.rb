bca11603924f0af7b4f176c4eea189e07d94e4b8
Tiago Carvalho <tiago@metatiago.com>
2013-03-02 16:55:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citgraph::Application.config.secret_token = '03ae1a20bcb941a9c69f2bb5a3b6480d6e38139351c8abf5998b19722016ffb3115c3c8ec01b5407ec0ffd348abcc75dc942adb7ddb529de81b243bad6d5a557'
